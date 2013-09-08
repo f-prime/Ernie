0184864906a84522e8411f657be36bc63157959b
Frankie <max00355@gmail.com>
2013-09-07 21:19:35 -0400

    Added exit() to errors

--- src/errors.py
+++ src/errors.py
@@ -1,14 +1,19 @@
 def syntax(code, line):
     print "Invalid Syntax: \""+ code+ "\" line: ", str(line+1)
+    exit()
 
 def typeerror(code, line):
     print "Type Error: \""+ code+ "\" line: ", str(line+1)
+    exit()
 
 def undefined(code):
     print "Undefined: "+code
+    exit()
 
 def invalidcall(code, line):
     print "Invalid Call: \""+ code+ "\" line: ", str(line+1)
+    exit()
 
 def nofile(code, line):
     print "File Nonexistant: \""+ code+ "\" line: ", str(line+1)
+    exit()
